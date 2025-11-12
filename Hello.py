edeae67d645cc934f387639604922c7fe771db0a
pjpope <pjpope@live.com>
2025-11-12 12:10:36 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,11 @@
-Kyle Pope
-11/12/2025
-A simpole Python program to print a message
+#Kyle Pope
+#11/12/2025
+#A simpole Python program to print a message
+
+#Kyle Pope
+#11/14/2025
+#added new message
 
 print("Hello, World!")
+
+Print("Welcome to GitHub!")
